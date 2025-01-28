4112b87e3515e6d1160aa384989f3f0a77e43124
bratiu <bratiu@faculty.ie.edu>
2025-01-28 18:35:15 +0100

    improved the first part

--- decode_start.py
+++ decode_start.py
@@ -1,2 +1,6 @@
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowel = "aeiou"
+message = ""
+with open("secret.txt") as f:
+    for line in f:
+        print(line) # instead of print, you should start decoding
