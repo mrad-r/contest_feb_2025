a2cd8cc64697be2b737fe613b48e8b470edfc4ab
matilderibeiro <matilde.csribeiro@gmail.com>
2025-01-31 13:19:53 +0100

    contest

--- decode_start.py
+++ decode_start.py
@@ -1,6 +1,22 @@
+from itertools import count
+
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        # print(line) #instead of print, you should start decoding
+        num_vowels = 0
+        for letter in line:
+            if letter in vowel: #letter is a vowel
+                num_vowels += 1
+
+        message += alphabet[num_vowels]
+    print(message)
+
+
+
+
+
+
+
